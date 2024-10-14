68a54a38184f049f2c49bcc67665a4119736b7fd
Andrew Holcomb <atholcomb@outlook.com>
2024-10-13 20:58:58 -0600

    adding credit_card project

---
+++ credit_card/credit_card_2.0/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# written by: Grace Holcomb
+# main.py
+# calls the issuer.py program to instantiate a new credit card
+
+from issuer import get_cc_number
+
+def main():
+  f = open("visa_cc.rsa", "w")
+  f.write(get_cc_number())
+  f.close()
+
+if __name__ == "__main__":
+        main()
