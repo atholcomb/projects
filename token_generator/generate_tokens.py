4e9b0dd14e16c4ae66cae3c449743477474330f1
Andrew Holcomb <atholcomb@outlook.com>
2024-11-18 23:05:59 -0700

    generate_tokens

--- token_generator/generate_tokens.py
+++ token_generator/generate_tokens.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 # written by: atholcomb
 # generate_tokens.py
-# Generates tokens of the quantity 
-# specified from tokens module
+# Generates tokens of the quantity specified from tokens module
 
 from tokens import create_tokens
 
@@ -10,4 +9,4 @@ def main():
   return create_tokens(15)
 
 if __name__ == "__main__":
-  print(main())
+    print(main())
