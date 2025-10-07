99200c9be88fb46a9b47ef981d28c4b569a4d49b
Andrew Holcomb <atholcomb@outlook.com>
2025-10-07 01:55:34 -0600

    acronyms

--- acronym_solver/solve_acronyms.py
+++ acronym_solver/solve_acronyms.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python3
 # written by: atholcomb
 # solve_acronyms.py
-# Finds and solves the acronyms within the acronyms.py module
-# Writes out the acronym to the left-hand side of the screen
+# prints out the acronym for a given string
 
 from acronyms import acronyms_data
 
 def main():
   # cleansed_data stores the (good) non-hyphenated dataset
-  cleansed_data = []
+  cleansed_data = [values.replace('-', ' ').split() for values in acronyms_data().values()]
 
-  for values in acronyms_data().values():
-    # replaces all '-' with a white space character
-    # split() creates a indexed based list for each string
-    cleansed_data.append(values.replace('-', ' ').split())
-    
-  for cleansed in cleansed_data:
-    for idx, data in enumerate(cleansed):
-      # store & uppercase the 'true' acronym inside the variable
-      acronym = data[0].upper()
-      print(acronym, data)
-      # move to next acronym in list once list reaches final index
-      if idx == (len(cleansed)-1):
-        print()
+  for cleansed_str in cleansed_data:
+    for idx, string in enumerate(cleansed_str):
+      # acronym creates the initial characters from the string
+      # using the 0th index and uppercases each 0th character
+      # ex: Today I Learned -> TIL
+      acronym = cleansed_str[idx][0].upper()
+      print(acronym, string)
+    print()
       
 if __name__ == "__main__":
-    main()
+      print(main())
