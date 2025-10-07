90226b3daa224a17aa6cc6b23eb22ac0691dab4a
Andrew Holcomb <atholcomb@outlook.com>
2025-10-06 23:50:35 -0600

    temperature_conversion

---
+++ temperature_conversion/README.md
@@ -0,0 +1,22 @@
+## Temperature Conversion
+
+### Converts a given temperature from Celsius into Fahrenheit and Kelvin
+
+## To Run:
+
+`./main.py`
+
+## Output should show as follows:
+`
+Celsius		Fahrenheit	Kelvin
+1649°C		3000°F		1922°K
+2160°C		3920°F		2433°K
+1395°C		2543°F		1668°K
+2160°C		3920°F		2433°K
+2270°C		4118°F		2543°K
+55°C		131°F		328°K
+1558°C		2836°F		1831°K
+1815°C		3299°F		2088°K
+1125°C		2057°F		1398°K
+1285°C		2345°F		1558°K
+`
---
+++ temperature_conversion/main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# written by: atholcomb
+# main.py
+
+from Conversion import Temperature
+from random import randrange
+
+""" Print out header row table """
+print("Celsius\t\tFahrenheit\tKelvin")
+
+def main():
+  for i in range(10):
+    """
+    The for loop creates ten instances of the Temperature object
+    -temp- stores a random temporary temp value to pass into the object
+    T creates the Temperature object and passes in a random temp range
+    The convert() method converts celsius in fahrenheit and kelvin
+    """
+    temp = randrange(1, 3000) 
+    T = Temperature(temp) 
+    print(T.convert()) 
+
+if __name__ == "__main__":
+        main()
