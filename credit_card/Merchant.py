cfe9c395e819291ca407db786ac2b72ba62aba5c
Andrew Holcomb <atholcomb@outlook.com>
2025-10-09 23:46:16 -0600

    credit card

--- credit_card/Merchant.py
+++ credit_card/Merchant.py
@@ -1,24 +1,46 @@
 #!/usr/bin/python3
 # written by: atholcomb
 # Merchant.py
+# program generates a debit card with a cvc code and exp date
 
-from Visa import Merchant
+from random import randint
 
-def main():
-    name = input("Enter the name for the card to be isssued: ") 
+class Merchant:
+    def __init__(self, owner="Andrew Holcomb"):
+      self.owner = owner 
+   
+    """ generates credit card number """
+    def cc_number(self):
+      cc_value_1 = ''
+      cc_value_2 = ''
+      cc_value_3 = ''
+      cc_value_4 = ''
 
-    # create credit card object
-    card = Merchant(name)
-  
-    # write out the card details to card_issued.rsa
-    with open("card_issued.rsa", "w") as f:
-      f.write("Visa Debit Card\n")
-      f.write(f'{card.owner}\n{card.cc_number()}\nEXP:{card.exp_date()} CVC:{card.cvc_code()}')
-    
-    # read the card details and output back to user
-    with open("card_issued.rsa", "r") as f:
-      data = f.read()
-      print(data)
+      for i in range(4):
+        # 43 standardizes on the Visa credit card format
+        cc_value_1 = str(43) + str(randint(0,9)) + str(randint(0,9))
+        # The remaining values make up the credit card with real numbers
+        cc_value_2 += str(randint(0,9))
+        cc_value_3 += str(randint(0,9))
+        cc_value_4 += str(randint(0,9))
 
-if __name__ == "__main__":
-         main()
+      return f"{cc_value_1}-{cc_value_2}-{cc_value_3}-{cc_value_4}"
+
+    """ generates expiration date """
+    def exp_date(self):
+      month = ''
+      year = ''
+
+      month = str(randint(1, 12))
+      year = str(randint(25, 30))
+
+      return f"{month}/{year}"
+
+    """ generates cvc number """
+    def cvc_code(self):
+      cvc = ''
+
+      for i in range(3):
+        cvc += str(randint(0,9)) 
+
+      return f"{cvc}"
