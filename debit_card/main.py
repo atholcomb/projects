5b724852f118a23b52ad2743eb6b26f9b0d23a3a
Andrew Holcomb <atholcomb@outlook.com>
2026-03-25 08:30:30 -0600

    debit_card

--- debit_card/main.py
+++ debit_card/main.py
@@ -5,12 +5,12 @@
 from Merchant import Merchant
 
 def main():
-    c = Merchant("Paul Holcomb")
+    c = Merchant()
   
     # write out the card details to card_issued.rsa
     with open("card_issued.rsa", "w") as f:
       f.write("Visa Debit Card\n")
-      f.write(f'{c.owner}\n{c.cc_number()}\nexp:{c.exp_date()} cvc:{c.cvc_code()}')
+      f.write(f'{c.owner}\n{c.cc_number()}\nexp:{c.exp_date()} cvc:{c.cvc_code()}\n')
     
     # open card_issued.rsa and show debit card information
     with open("card_issued.rsa", "r") as f:
